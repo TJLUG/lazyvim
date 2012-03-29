58aae94e90860b6a371324e83b974540102cf55b
Richard Ma <richard.ma.19850509@gmail.com>
2012-03-30 00:19:16 +0800

    rewrite install.sh (atom)

--- install.sh
+++ install.sh
@@ -1,14 +1,67 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -o errexit
 
-if [ -e $HOME/.vimrc ]; then
-    echo "BACKUP $HOME/.vimrc -> $HOME/.vimrc.old"
-    mv $HOME/.vimrc $HOME/.vimrc.old
-fi
-if [ -e $HOME/.vim/vimrc.d ]; then
-    echo "BACKUP $HOME/.vim/vimrc.d -> $HOME/.vim/vimrc.d.old"
-    mv $HOME/.vimrc/vimrc.d $HOME/.vim/vimrc.d.old
+suffix="bak" # backup file suffix
+
+function backup_action
+{
+    if [[ -e "$HOME/.$1" ]]; then
+        echo "BACKUP $HOME/.$1 -> $HOME/.$1.$suffix"
+        # backup file already exists.
+        if [[ -e "$HOME/.$1.$suffix" ]]; then
+            echo "ERROR: $HOME/.$1.$suffix EXISTS."
+            echo "DELETE OR ROLLBACK, then run this script again."
+            return 1 # exit and terminat script.
+        fi
+        mv "$HOME/.$1" "$HOME/.$1.$suffix"
+    fi
+
+    return 0
+}
+
+function handle_action
+{
+    # backup old configuration
+    backup_action "$1"
+
+    echo "Installing $PWD/$1 -> $HOME/.$1"
+    ln -s "$PWD/$1" "$HOME/.$1"
+    
+    return 0
+}
+
+function rollback_action
+{
+    echo "$HOME/.$1.$suffix"
+    if [[ -e "$HOME/.$1.$suffix" ]]; then
+        echo "ROLLBACK $HOME/.$1.$suffix -> $HOME/.$1"
+        if [[ -e "$HOME/.$1" ]]; then
+            rm -f "$HOME/.$1"
+        fi
+        mv "$HOME/.$1.$suffix" "$HOME/.$1"
+    fi
+
+    return 0
+}
+
+function rollback
+{
+    echo "START ROLLBACK!!"
+    rollback_action vimrc
+    rollback_action vim
+    echo "ROLLBACK COMPLETED!!"
+}
+
+if [[ "$1" == "rollback" ]]; then
+    rollback # this will cause an error and rollback function will run
+    exit 0
 fi
 
-ln -s $PWD/vimrc $HOME/.vimrc # install .vimrc
-ln -s $PWD/vimrc.d $HOME/.vim/ # install vimrc programming language pack
+# install progress (atom)
+trap "rollback" INT TERM EXIT
+
+handle_action vimrc
+handle_action vim
+
+trap - INT TERM EXIT
+exit 0
